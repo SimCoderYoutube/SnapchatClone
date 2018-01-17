644667874f28f8f1e44dcab6da1730fcbbdeb9fd
simcoder <info@simcoder.com>
2018-01-17 14:24:53 +0000

    - Saving image to firebase storage

--- app/src/main/java/com/simcoder/snapchatclone/ShowCaptureActivity.java
+++ app/src/main/java/com/simcoder/snapchatclone/ShowCaptureActivity.java
@@ -3,11 +3,31 @@ package com.simcoder.snapchatclone;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShowCaptureActivity extends AppCompatActivity {
+
+    String Uid;
+    Bitmap rotateBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +43,70 @@ public class ShowCaptureActivity extends AppCompatActivity {
 
             Bitmap decodedBitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
 
-            Bitmap rotateBitmap = rotate(decodedBitmap);
-
+            rotateBitmap = rotate(decodedBitmap);
 
             image.setImageBitmap(rotateBitmap);
         }
+
+        Uid = FirebaseAuth.getInstance().getUid();
+
+        Button mStory = findViewById(R.id.story);
+        mStory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToStories();
+            }
+        });
+
+
     }
+
+    private void saveToStories() {
+        final DatabaseReference userStoryDb = FirebaseDatabase.getInstance().getReference().child("users").child(Uid).child("story");
+        final String key = userStoryDb.push().getKey();
+
+        StorageReference filePath = FirebaseStorage.getInstance().getReference().child("captures").child(key);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        rotateBitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+        byte[] dataToUpload = baos.toByteArray();
+        UploadTask uploadTask = filePath.putBytes(dataToUpload);
+
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri imageUrl = taskSnapshot.getDownloadUrl();
+
+                Long currentTimestamp = System.currentTimeMillis();
+                Long endTimestamp = currentTimestamp + (24*60*60*1000);
+
+                Map<String, Object> mapToUpload = new HashMap<>();
+                mapToUpload.put("imageUrl", imageUrl.toString());
+                mapToUpload.put("timestampBeg", currentTimestamp);
+                mapToUpload.put("timestampEnd", endTimestamp);
+
+                userStoryDb.child(key).setValue(mapToUpload);
+
+                finish();
+                return;
+            }
+        });
+
+
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                finish();
+                return;
+            }
+        });
+
+    }
+
+
+
+
 
     private Bitmap rotate(Bitmap decodedBitmap) {
         int w = decodedBitmap.getWidth();
